98838f744f09553a71ab8c9bbf7c95147d5b7a8e
DavNoronha <davidnd97@gmail.com>
2022-08-01 13:28:37 -0300

    criei uma pagina de calculo imc

---
+++ pages/calculadoraIMC.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-text-field v-model="altura" type="number" outlined label="Altura"/>
+        <v-text-field v-model="peso" type="number" outlined label="Peso"/>
+        <v-btn @click="calcula">Calcular IMC</v-btn>
+        <p>{{resultado}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'calculadora-imc',
+  data() {
+    return {
+      resultado: null,
+      altura: null,
+      peso: null,
+    }
+  },
+  computed: {},
+  methods: {
+    calcula () {
+        this.resultado = this.peso/(this.altura*this.altura)
+    }
+  }
+}
+</script>
